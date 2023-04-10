7974f58eff10a40b7815cc3466a49c39bc094381
Zachary <90229567+Zachary-Wayne@users.noreply.github.com>
2023-04-09 20:10:52 -0400

    Fixed some styling

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -19,10 +19,11 @@ function addRoom() {
 }
 
 document.getElementById("input").addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
+    if (event.key === "Enter") {
         let request = {"type":"chat", "msg":event.target.value};
         ws.send(JSON.stringify(request));
-        event.target.value = "";
+        event.target.value = ""; // Clear input box
+        //sendMessage(message)
     }
 });
 
@@ -30,4 +31,16 @@ function timestamp() {
     let d = new Date(), minutes = d.getMinutes();
     if (minutes < 10) minutes = '0' + minutes;
     return d.getHours() + ':' + minutes;
+}
+
+function sendMsg (msg){
+    //var name = ;
+    //var date = ;
+    //var time = ;
+    //document
+    //.getElementById("chat_table")
+    //    .insertRow(-1) // Last index
+    //    .insertCell(0)
+    //    .innerHTML = "<h1>"+user_name+"</h1><h2>"+time+"</h2><p>"+element.value+"</p>";
+    //window.scrollTo(0, document.body.scrollHeight); // Scroll the screen to the bottom so the new message can be seen.
 }
